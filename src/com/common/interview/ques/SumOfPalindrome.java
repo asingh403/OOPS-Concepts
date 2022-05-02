98a7d56f2632e3b2ef1c6c3812fd7303f4de92e3
Ashutosh Singh <asingh.it@hotmail.com>
2022-05-02 12:16:10 +0530

    Update SumOfPalindrome.java

--- src/com/common/interview/ques/SumOfPalindrome.java
+++ src/com/common/interview/ques/SumOfPalindrome.java
@@ -5,7 +5,6 @@ public class SumOfPalindrome {
     public static String sumOfPalindrome(String str){
         int b = 0;
         int e = str.length()-1;
-
         while(b<e){
             if(str.charAt(b) != str.charAt(e)){
                 return "invalid palindrome";
@@ -13,10 +12,8 @@ public class SumOfPalindrome {
                 b++;
                 e--;
             }
-
         }
         return "valid palindrome";
-
     }
     public static void main(String[] args) {
 
@@ -24,5 +21,4 @@ public class SumOfPalindrome {
         String result  = sumOfPalindrome(input);
         System.out.println(result);
     }
-
 }
