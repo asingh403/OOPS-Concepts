5933ab781c28335d660137fd5677269f730c2b37
asingh403 <asingh.it@hotmail.com>
2022-05-08 01:29:36 +0530

    Builder Pattern

---
+++ src/com/gfg/strings/AnagramStrings.java
@@ -0,0 +1,32 @@
+package com.gfg.strings;
+
+import java.util.Arrays;
+
+public class AnagramStrings {
+
+    private static final int CHAR = 256;
+    public  static boolean findAnagramStr(String s1, String s2){
+
+        if(s1.length() != s2.length()) return false;
+        int [] count = new int[CHAR];
+
+        for(int i=0; i<s1.length();i++){
+            count[s1.charAt(i)]++;
+            System.out.println(count[s1.charAt(i)]++);
+            count[s2.charAt(i)]--;
+            System.out.println(count[s2.charAt(i)]--);
+        }
+        for (int i=0; i<CHAR; i++){
+            if(count[i] != 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "listen";
+        String s2 = "silent";
+
+        boolean result = findAnagramStr(s1, s2);
+        System.out.println(result);
+    }
+}
